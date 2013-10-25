64e132b335ebc0e733cab8ab9cb7fb796fc62713
sajeerjaleel <sajeerj@qburst.com>
2013-10-25 16:32:26 +0530

    Delete execute.rb

--- execute.rb
+++
@@ -1,9 +0,0 @@
-require 'net/http'
-require 'mysql2'
-require_relative 'xml'
-pass_url = "www.qburst.com"
-pass_ext =  "/sitemap.xml" 
-xml = Xml.new(pass_url,pass_ext)
-xml.configure_database
-xml.fetch_remote_data
-xml.parse_data
