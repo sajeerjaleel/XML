f9a5c0589016d3888c6f989396bb8445e4b3642c
sajeerjaleel <sajeerj@qburst.com>
2013-10-29 18:06:52 +0530

    Update nokogiri.rb

--- nokogiri.rb
+++ nokogiri.rb
@@ -7,54 +7,54 @@
 class Xml
 
 =begin
-	input : url
-	output : @xml_doc
-	pass url and get xml contents inside the url
+  input : url
+  output : @xml_doc
+  pass url and get xml contents inside the url
 =end
   def initialize(url)
     @xml_doc  = Nokogiri::XML(open(url))
   end
 
 =begin
-	input : nil 
-	output : @client
-	Create connection with database
+  input : nil 
+  output : @client
+  Create connection with database
 =end
   def configure_database
-	begin
-	  @client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "qburst", :database => "ruby")
-	rescue Exception=>e
-	  puts e.message
-	end
+    begin
+      @client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "qburst", :database => "ruby")
+    rescue Exception=>e
+      puts e.message
+    end
   end
   
 =begin
-	input : @xml_doc
-	output : @text
-	Fetch all datas inside the <loc> node
+  input : @xml_doc
+  output : @text
+  Fetch all datas inside the <loc> node
 =end
   def fetch_remote_data
     @doc = @xml_doc. remove_namespaces!() 
-		@text = @doc.search('url/loc').xpath('text()')
+    @text = @doc.search('url/loc').xpath('text()')
   end
 
 =begin
-	input : @client
-	output : @insert
-	Insert the data into the database
+  input : @client
+  output : @insert
+  Insert the data into the database
 =end
   def parse_data
-		if @client != nil
-	  	begin
-	    	@text.each do |content|
-				@insert = @client.query("INSERT INTO Xml (xml) VALUES ('#{ content }')")
-	  	end
+    if @client != nil
+      begin
+        @text.each do |content|
+	@insert = @client.query("INSERT INTO Xml (xml) VALUES ('#{ content }')")
+      end
 	  	
-	  	rescue Exception=>e
-				puts e.message
-	  	ensure
-				@client.close
-	  	end
-		end
+      rescue Exception=>e
+	puts e.message
+      ensure
+	@client.close
+      end
+    end
   end
 end
