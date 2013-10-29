5a74147adc6969f5c2265ed10aa02d158191753a
sajeerjaleel <sajeerj@qburst.com>
2013-10-29 17:59:12 +0530

    Update spider.rb

--- spider.rb
+++ spider.rb
@@ -4,28 +4,28 @@ require 'mysql2'
 $client = Mysql2::Client.new(:host => "localhost", :username => "root", :password => "qburst", :database => "ruby")
 
 =begin
-	input : nil
-	output : url
-	fetches category name and passes category url
+  input : nil
+  output : url
+  fetches category name and passes category url
 =end
 def categories
-	$count = nil
-	page = Nokogiri::HTML(open("http://www.1mobile.com/downloads/"))  
+  $count = nil
+  page = Nokogiri::HTML(open("http://www.1mobile.com/downloads/"))  
 	
-	page.css('div.c a').each do |a|
-		#The top 6 Download list are removed		
-		if a.text.strip == "Action"
-			$count = 1
-		end
-		#The loop begins only after the category 'Action'
-		if $count
-			category = a.text.strip
-			url = a["href"]
-			$client.query("INSERT INTO categories (name) VALUES ('#{ category }')")
-			name(url,category)
-		end
+  page.css('div.c a').each do |a|
+    #The top 6 Download list are removed		
+    if a.text.strip == "Action"
+      $count = 1
+    end
+    #The loop begins only after the category 'Action'
+    if $count
+      category = a.text.strip
+      url = a["href"]
+      $client.query("INSERT INTO categories (name) VALUES ('#{ category }')")
+      name(url,category)
+    end
 	
-	end
+  end
 end
 
 =begin
@@ -35,17 +35,17 @@ end
 =end
 def name(url,category)
 
-	#Category page
-	pages = Nokogiri::HTML(open("http://www.1mobile.com"+url)) 
+  #Category page
+  pages = Nokogiri::HTML(open("http://www.1mobile.com"+url)) 
 		
-	pages.css('div.searchapps li p a').each do |a|
-		app_name = a.text.strip.gsub("'","")
-		url_app = a["href"]
-		url_app = url_app.gsub("[free]-","")
-		contents(url_app,app_name,category)
-	end
+  pages.css('div.searchapps li p a').each do |a|
+    app_name = a.text.strip.gsub("'","")
+    url_app = a["href"]
+    url_app = url_app.gsub("[free]-","")
+    contents(url_app,app_name,category)
+  end
 
-	sleep 2
+  sleep 2
 end
 
 =begin
@@ -55,18 +55,18 @@ end
 =end
 def contents(url_app,app_name,category)
 
-	#Application page
-	page = Nokogiri::HTML(open("http://www.1mobile.com"+url_app))
+  #Application page
+  page = Nokogiri::HTML(open("http://www.1mobile.com"+url_app))
 
-	#fetching Description
-	description = page.css('div.simpleinfo').text.strip.gsub("'","")
-	$client.query("INSERT INTO applications (Description,categories,Name) VALUES ('#{ description }','#{ category }','#{ app_name }')")
+  #fetching Description
+  description = page.css('div.simpleinfo').text.strip.gsub("'","")
+  $client.query("INSERT INTO applications (Description,categories,Name) VALUES ('#{ description }','#{ category }','#{ app_name }')")
 
-	#fetching screen shots
-	page.css('div.detailslidercnt a img').each do|a|
-		image_url = a["src"]
-		$client.query("INSERT INTO screen_shots (application,Image_URL) VALUES ('#{ app_name }','#{ image_url }')")
-	end
+  #fetching screen shots
+  page.css('div.detailslidercnt a img').each do|a|
+    image_url = a["src"]
+    $client.query("INSERT INTO screen_shots (application,Image_URL) VALUES ('#{ app_name }','#{ image_url }')")
+  end
 
 end
 
