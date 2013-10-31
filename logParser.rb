b21a949d29bb9e3c61bbd92daf49bd000cbdbcf2
sajeerjaleel <sajeerj@qburst.com>
2013-10-31 17:15:22 +0530

    Update logParser.rb

--- logParser.rb
+++ logParser.rb
@@ -1,87 +1,90 @@
 class LogParser
 
 =begin
-input : nil
-output : $file
-Read contents of a file to a string
+  input : nil
+  output : $file
+  Read contents of a file to a string
 =end
-	def initialize path
-	  $file = File.read(path)
-	  @count = Hash.new(0)
-	end
+  def initialize path
+    $file = File.read(path)
+    @count = Hash.new(0)
+  end
 
 =begin
-input: $file
-output : @count
-Count total request grouped by date and hour
+  input: $file
+  output : @count
+  Count total request grouped by date and hour
 =end
-	def date_time 
-		$file.each_line do|line|
-			time = /\d{1,2}\/[A-Z][a-z][a-z]\/\d{4}\:\d{1,2}/.match line
-			if time
-         @count[time[0]] += 1
+  def date_time 
+    $file.each_line do|line|
+      time = /\d{1,2}\/[A-Z][a-z][a-z]\/\d{4}\:\d{1,2}/.match line
+      if time
+        @count[time[0]] += 1
       end
-		end 
-	end
+    end 
+  end
 
 =begin
-input : $file
-output : slowest, @url, val1, val2
-Find the slowest time groupd by date and then find corresponding URL
+  input : $file
+  output : slowest, @url, val1, val2
+  Find the slowest time groupd by date and then find corresponding URL
 =end
-	def date_time_url
-		@slowest_time= 0
-		print "\nTotal requests grouped by the Date/hour\n\n"
+  def date_time_url
+    @slowest_time= 0
+    print "\nTotal requests grouped by the Date/hour\n\n"
 		
-		#taking each date&hour
-		@count.each do|val1,val2|
-			slowest = 0
-			#searching on each line to find slowest
-			$file.each_line do|line|
-				if line.include? val1
-					time = line.match /\d+$/
-					if time
-						#finding slowest time
-						time = time[0].to_i
-						if time > slowest
-							slowest = time
-						end
-					end
-				end
-				slowest = slowest.to_s
-				#finding slowest url in corresponding date
-				if line.include? slowest
-					@url = line.match /https?:\/\/[\S]+/.to_s
-				end
-				slowest = slowest.to_i
-			end
-			request = "request"
-			#checking plural form of request
-			if val1.to_i > 1
-				request += "s"
-			end 
-			print "#{ val1 } : #{val2} #{request} \tSlowest-Time : #{ slowest} \tSlowest-URL : #{ @url }\n\n"
-			if slowest > @slowest_time
-				@slowest_time = slowest
-			end
-		end 
+    #taking each date&hour
+    @count.each do|val1,val2|
+      slowest = 0
+      
+      #searching on each line to find slowest
+      $file.each_line do|line|
+	if line.include? val1
+	  time = line.match /\d+$/
+	  if time
+	    #finding slowest time
+	    time = time[0].to_i
+	    if time > slowest
+	      slowest = time
+	    end
+	  end
 	end
+	slowest = slowest.to_s
+	#finding slowest url in corresponding date
+	if line.include? slowest
+	  @url = line.match /https?:\/\/[\S]+/.to_s
+	end
+	slowest = slowest.to_i
+      end
+      
+      request = "request"
+      #checking plural form of request
+      if val1.to_i > 1
+	request += "s"
+      end 
+      print "#{ val1 } : #{val2} #{request} \tSlowest-Time : #{ slowest} \tSlowest-URL : #{ @url }\n\n"
+      if slowest > @slowest_time
+        @slowest_time = slowest
+      end
+    end 
+    
+  end
 
 =begin
-input : $file, @slowest_time
-output : @url
-Find the slowest URL corresponding to slowest time
+  input : $file, @slowest_time
+  output : @url
+  Find the slowest URL corresponding to slowest time
 =end
-	def slowest_URL	
-		#find corresponding URL 
-		@slowest_time = @slowest_time.to_s
-		$file.each_line do|line|
-			if line.include? @slowest_time
-				@slowest_url = line.match /https?:\/\/[\S]+/.to_s
-			end
-		end	
-		puts "\nSlowest URL : #{ @slowest_url }\n "
-	end
+  def slowest_URL	
+    #find corresponding URL 
+    @slowest_time = @slowest_time.to_s
+    $file.each_line do|line|
+      if line.include? @slowest_time
+	@slowest_url = line.match /https?:\/\/[\S]+/.to_s
+      end
+    end	
+    puts "\nSlowest URL : #{ @slowest_url }\n "
+  end
 
 end
 
